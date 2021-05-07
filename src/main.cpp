bbe2bcf615facaaa81006fc7646ddd3e49cb9ebd
Critteros <szymonkoziol3@gmail.com>
2021-05-07 20:16:31 +0200

    Init commit

---
+++ src/main.cpp
@@ -0,0 +1,44 @@
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+#include <fmt/core.h>
+#include <fmt/color.h>
+
+
+int main(void)
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(1280, 720, "My window", NULL, NULL);
+
+    fmt::print(fg(fmt::color::crimson) | fmt::emphasis::bold,
+        "Hello, {}!\n", "world");
+
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
