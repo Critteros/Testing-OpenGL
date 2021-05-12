aa528bc4e33cd9058d14e0973bb76a05a22b4f16
Critteros <Critteros@users.noreply.github.com>
2021-05-12 09:34:38 +0200

    Cahanged glad to GL4.3 and added message callback

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,52 @@
 #include <GLFW/glfw3.h>
 #include <glad/glad.h>
 #include <fmt/core.h>
+#include <fmt/color.h>
 
 #include "types.h"
-#include "timer.h"
+
 
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 800
+
+
+void GLAPIENTRY
+MessageCallback(GLenum source,
+    GLenum type,
+    GLuint id,
+    GLenum severity,
+    GLsizei length,
+    const GLchar* message,
+    const void* userParam)
+{
+    
+    
+    switch (severity)
+    {
+    case(GL_DEBUG_SEVERITY_HIGH):
+        fmt::print(fg(fmt::color::crimson),
+            "[GL_HIGH]: type = {} \n\t{}\n", type, message);
+    break;
+
+    case(GL_DEBUG_SEVERITY_MEDIUM):
+        fmt::print(//fg(fmt::color::yellow),
+            "[GL_MEDIUM]: type = {} \n\t{}\n", type, message);
+    break;
+
+    case(GL_DEBUG_SEVERITY_LOW):
+        fmt::print(fg(fmt::color::light_green),
+            "[GL_LOW]: type = {} \n\t{}\n", type, message);
+    break;
+
+    case(GL_DEBUG_SEVERITY_NOTIFICATION):
+        fmt::print(//fg(fmt::color::silver),
+            "[GL_INFO]: type = {} \n\t{}\n", type, message);    
+    break;
+    }
+}
+    
+    
+
 
 
 int main(void)
@@ -19,7 +59,7 @@ int main(void)
         return -1;
 
     //Providing OpenGL hint to GLFW
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
@@ -43,12 +83,92 @@ int main(void)
     //Loading OpenGL mappings
     gladLoadGL();
 
+    //Debug messages
+    glEnable(GL_DEBUG_OUTPUT);
+    glDebugMessageCallback(MessageCallback, 0);
+
     //Initial ViewPort size for OpenGL to render in
     glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
 
 
     std::cout << "OpenGl Version: " << glGetString(GL_VERSION) << std::endl;
+
+
+    const char* vertexShaderSource =
+        "#version 330 core\n"
+        "layout (location = 0) in vec4 positions;\n"
+        "void main()\n"
+        "{\n"
+        "   gl_Position = positions;\n"
+        "}\0";
+
+    const char* fragmentShaderSource =
+        "#version 330 core\n"
+        "\n"
+        "uniform vec4 m_color;\n"
+        "layout(location = 0) out vec4 color;\n"
+        "\n"
+        "void main()\n"
+        "{\n"
+        "   color = m_color;\n"
+        "}\n";
+
+
+
+    //Compiling Vertex shader
+    u32 v_sh = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(v_sh, 1, &vertexShaderSource, NULL);
+    glCompileShader(v_sh);
+
+    i32 vertex_result;
+    glGetShaderiv(v_sh, GL_COMPILE_STATUS, &vertex_result);
+    if (vertex_result == GL_FALSE)
+    {
+        std::cout << "Error occured when compiling vertex shader" << std::endl;
+        int m_length;
+        glGetShaderiv(v_sh, GL_INFO_LOG_LENGTH, &m_length);
+        char* message = (char*)malloc(m_length * sizeof(char));
+        glGetShaderInfoLog(v_sh, m_length, &m_length, message);
+        std::cout << message << std::endl;
+        free(message);
+    }
     
+    //Compiling Fragment shader
+    u32 f_sh = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(f_sh, 1, &fragmentShaderSource, nullptr);
+    glCompileShader(f_sh);
+
+    i32 fragment_result;
+    glGetShaderiv(f_sh, GL_COMPILE_STATUS, &fragment_result);
+    if (fragment_result == GL_FALSE)
+    {
+        std::cout << "Error occured when compiling vertex shader" << std::endl;
+        int m_length;
+        glGetShaderiv(f_sh, GL_INFO_LOG_LENGTH, &m_length);
+        char* message = (char*)malloc(m_length * sizeof(char));
+        glGetShaderInfoLog(f_sh, m_length, &m_length, message);
+        std::cout << message << std::endl;
+        free(message);
+    }
+
+    u32 program = glCreateProgram();
+
+    glAttachShader(program, v_sh);
+    glAttachShader(program, f_sh);
+    glLinkProgram(program);
+    glValidateProgram(program);
+
+    i32 success;
+    char infoLog[512];
+
+    glGetProgramiv(program, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(program, 512, NULL, infoLog);
+
+    }
+
+
+    //glUseProgram(program);
 
 
     f32 data1[] =
@@ -142,98 +262,22 @@ int main(void)
     glBufferData(GL_ARRAY_BUFFER, sizeof(data4), data4, GL_STATIC_DRAW);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
 
+    
 
 
 
+    
 
-    const char* vertexShaderSource =
-        "#version 330 core\n"
-        "layout (location = 0) in vec3 aPos;\n"
-        "void main()\n"
-        "{\n"
-        "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-        "}\0";
-
-    std::string fragmentShader =
-        "#version 330 core\n"
-        "\n"
-        "uniform vec4 m_color;\n"
-        "layout(location = 0) out vec4 color;\n"
-        "\n"
-        "void main()\n"
-        "{\n"
-        "   color = m_color;\n"
-        "}\n";
-
-
-
-    //Compiling Vertex shader
-    u32 v_sh = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(v_sh, 1, &vertexShaderSource, NULL);
-    glCompileShader(v_sh);
-
-    i32 vertex_result;
-    glGetShaderiv(v_sh, GL_COMPILE_STATUS, &vertex_result);
-    if (vertex_result == GL_FALSE)
-    {
-        std::cout << "Error occured when compiling vertex shader" << std::endl;
-        int m_length;
-        glGetShaderiv(v_sh, GL_INFO_LOG_LENGTH, &m_length);
-        char* message = (char*)malloc(m_length * sizeof(char));
-        glGetShaderInfoLog(v_sh, m_length, &m_length, message);
-        std::cout << message << std::endl;
-        free(message);
-    }
-
-    //Compiling Fragment shader
-    u32 f_sh = glCreateShader(GL_FRAGMENT_SHADER);
-    const char* f_sh_source = fragmentShader.c_str();
-    glShaderSource(f_sh, 1, &f_sh_source, nullptr);
-    glCompileShader(f_sh);
-
-    i32 fragment_result;
-    glGetShaderiv(f_sh, GL_COMPILE_STATUS, &fragment_result);
-    if (fragment_result == GL_FALSE)
-    {
-        std::cout << "Error occured when compiling vertex shader" << std::endl;
-        int m_length;
-        glGetShaderiv(f_sh, GL_INFO_LOG_LENGTH, &m_length);
-        char* message = (char*)malloc(m_length * sizeof(char));
-        glGetShaderInfoLog(f_sh, m_length, &m_length, message);
-        std::cout << message << std::endl;
-        free(message);
-    }
-
-    u32 program = glCreateProgram();
-
-    glAttachShader(program, v_sh);
-    glAttachShader(program, f_sh);
-    glLinkProgram(program);
-    glValidateProgram(program);
-
-    i32 success;
-    char infoLog[512];
-
-    glGetProgramiv(program, GL_LINK_STATUS, &success);
-    if (!success) {
-        glGetProgramInfoLog(program, 512, NULL, infoLog);
-
-    }
-
-
-    glUseProgram(program);
+    
 
 
     int location = glGetUniformLocation(program, "m_color");
 
 
 
-    // Put this in your class somewhere
-    CTimer m_timer;
+ 
 
-    // Initialize the timer using
-    m_timer.Init();
-    m_timer.Update();
+    
 
     //Main loop
     while (!glfwWindowShouldClose(window))
@@ -241,8 +285,9 @@ int main(void)
         glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        m_timer.Update();
+       
         //Rendering
+        glUseProgram(program);
 
         glUniform4f(location, 0.863f, 0.078f, 0.235f, 1.0f);
         //Leg1
@@ -271,7 +316,8 @@ int main(void)
         glEnableVertexAttribArray(0);
         glDrawArrays(GL_TRIANGLES, 0, 2 * 3);
 
-        fmt::print("FPS: {}\n", (i32)m_timer.GetFPS());
+      
+        glUseProgram(0);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
